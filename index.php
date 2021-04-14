a2c4d8274215ac669c7e31f3d9d09dba97c6d452
banon jonathan <j.banon01@gmail.com>
2021-04-15 00:15:28 +0200

    finished

---
+++ index.php
@@ -0,0 +1,153 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css" >
+
+    <title>Mon précieux</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row"> <! 01>
+            <button type="button" class="btn btn-secondary col-1"></button>
+            <button type="button" class="btn btn-secondary col-1 offset-9"></button>
+        </div>
+        <div class="row"><! 02>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-danger col-1 offset-3"></button>
+            <button type="button" class="btn btn-warning col-1"></button>
+            <button type="button" class="btn btn-danger col-1"></button>
+            <button type="button" class="btn btn-secondary col-1 offset-3"></button>
+        </div>
+        <div class="row"><! 03>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-danger col-1 offset-2"></button>
+            <button type="button" class="btn btn-warning col-1"></button>
+            <button type="button" class="btn btn-warning col-1 offset-1"></button>
+            <button type="button" class="btn btn-danger col-1"></button>
+            <button type="button" class="btn btn-secondary col-1 offset-2"></button>
+        </div>
+        <div class="row"><! 04>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-danger col-1 "></button>
+            <button type="button" class="btn btn-danger col-1 "></button>
+            <button type="button" class="btn btn-warning col-1"></button>
+            <button type="button" class="btn btn-warning col-1 offset-1"></button>
+            <button type="button" class="btn btn-danger col-1"></button>
+            <button type="button" class="btn btn-danger col-1"></button>
+            <button type="button" class="btn btn-secondary col-1"></button>
+            <button type="button" class="btn btn-secondary col-1"></button>
+        </div>
+        <div class="row"><! 05>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-danger col-1 offset-1"></button>
+            <button type="button" class="btn btn-warning col-1 "></button>
+            <button type="button" class="btn btn-warning col-1 offset-1"></button>
+            <button type="button" class="btn btn-danger col-1"></button>
+            <button type="button" class="btn btn-secondary col-1 offset-1"></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+        </div>
+        <div class="row"><! 06>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-danger col-1 offset-1"></button>
+            <button type="button" class="btn btn-warning col-1"></button>
+            <button type="button" class="btn btn-danger col-1"></button>
+            <button type="button" class="btn btn-secondary col-1 offset-1"></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+        </div>
+        <div class="row"><! 07>
+            <button type="button" class="btn btn-secondary col-1 offset-1"></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 offset-3"></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+        </div>
+        <div class="row"><! 08>
+            <button type="button" class="btn btn-secondary col-1 offset-2"></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+        </div>
+        <div class="row"><! 09>
+            <button type="button" class="btn btn-secondary col-1 offset-3"></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+        </div>
+        <div class="row"><! 10>
+            <button type="button" class="btn btn-secondary col-1 offset-4"></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+        </div>
+
+        <div class="row"><! 11>
+            <button type="button" class="btn btn-secondary col-1 offset-4"></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+        </div>
+        <div class="row"><! 11>
+            <button type="button" class="btn btn-secondary col-1 offset-4"></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+        </div>
+        <div class="row"><! 11>
+            <button type="button" class="btn btn-secondary col-1 offset-4"></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+        </div>
+        <div class="row"><! 11>
+            <button type="button" class="btn btn-secondary col-1 offset-4"></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+        </div>
+        <div class="row"><! 11>
+            <button type="button" class="btn btn-secondary col-1 offset-4"></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+        </div>
+        <div class="row"><! 11>
+            <button type="button" class="btn btn-secondary col-1 offset-4"></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+        </div>
+        <div class="row"><! 11>
+            <button type="button" class="btn btn-secondary col-1 offset-4"></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+        </div>
+        <div class="row"><! 11>
+            <button type="button" class="btn btn-secondary col-1 offset-3"></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+        </div>
+        <div class="row"><! 11>
+            <button type="button" class="btn btn-secondary col-1 offset-2"></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+            <button type="button" class="btn btn-secondary col-1 "></button>
+        </div>
+    </div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+</body>
+</html>
